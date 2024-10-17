61c7c61111a854cecda61def9ec0222256c2bbb3
LoveAviation <ivangeorgmarcov@gmail.com>
2024-10-18 00:01:05 +0300

    раз раз

--- app/src/main/java/com/example/androidmaster2/MainActivity.kt
+++ app/src/main/java/com/example/androidmaster2/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.androidmaster2
 
 
+import android.annotation.SuppressLint
 import android.content.ComponentName
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.content.ServiceConnection
 import android.os.Build
@@ -9,34 +11,68 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.IBinder
 import android.os.Looper
+import android.os.Message
+import android.os.Messenger
+import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import com.example.androidmaster2.MainService.Companion.GET_CURRENT
+import com.example.androidmaster2.MainService.Companion.IS_RUNNING
+import com.example.androidmaster2.MainService.Companion.RESTART_TIMER
+import com.example.androidmaster2.MainService.Companion.START_TIMER
 import com.example.androidmaster2.databinding.ActivityMainBinding
 
 @RequiresApi(Build.VERSION_CODES.O)
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    private var myService: MainService? = null
+    private var myMessenger: Messenger? = null
+    private var myService: Messenger? = null
     private var isServiceBound = false
+    private var isCounterRunning = false
     private val handler = Handler(Looper.getMainLooper())
 
     private val connection = object : ServiceConnection {
         override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-            val binder = service as MainService.LocalBinder
-            myService = binder.getService()
+            myService = Messenger(service)
             isServiceBound = true
-
-            if (myService?.isRunning == true) {
-                binding.button.text = getString(R.string.restart_service)
-                startPolling()
-            } else {
-                binding.text.text = ""
-            }
+            sendMsgToService(IS_RUNNING)
         }
 
         override fun onServiceDisconnected(name: ComponentName?) {
+            myService = null
             isServiceBound = false
+        }
+    }
+
+    @SuppressLint("HandlerLeak")
+    inner class ResponseHandler(): Handler(Looper.getMainLooper()){
+        override fun handleMessage(msg: Message) {
+            when(msg.what){
+                START_TIMER -> {
+                    startPolling()
+                    isCounterRunning = true
+                    binding.button.text = getString(R.string.restart_service)
+                    updateUI(msg.arg1)
+                }
+                RESTART_TIMER -> {
+                    updateUI(msg.arg1)
+                }
+                GET_CURRENT -> {
+                    updateUI(msg.arg1)
+                }
+                IS_RUNNING -> {
+                    if (msg.arg1 == 1){
+                        isCounterRunning = true
+                        startPolling()
+                        binding.button.text = getString(R.string.restart_service)
+                    }else{
+                        isCounterRunning = false
+                        binding.button.text = getString(R.string.start_service)
+                    }
+                }
+                else -> super.handleMessage(msg)
+            }
         }
     }
 
@@ -45,16 +81,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.text.text = ""
+        myMessenger = Messenger(ResponseHandler())
         startService()
 
-        binding.button.setOnClickListener {
-            if (myService?.isRunning == true) {
-                binding.text.text = myService?.restartCountdown().toString()
-            } else {
-                binding.text.text = myService?.startCountdown().toString()
-                binding.button.text = getString(R.string.restart_service)
-                startPolling()
+        binding.button.setOnClickListener{
+            if (isCounterRunning){
+                sendMsgToService(RESTART_TIMER)
+            }else{
+                sendMsgToService(START_TIMER)
             }
         }
     }
@@ -68,16 +102,31 @@ class MainActivity : AppCompatActivity() {
     private fun startPolling() {
         handler.post(object : Runnable {
             override fun run() {
-                val counter = myService?.getCurrentProgress()
-                if (counter != null && counter > 0) {
-                    binding.text.text = counter.toString()
-                    handler.postDelayed(this, 1000)
-                } else {
-                    binding.button.text = getString(R.string.start_service)
-                    binding.text.text = ""
-                }
+                sendMsgToService(GET_CURRENT)
+                if (isCounterRunning) { handler.postDelayed(this, 1000) }
             }
         })
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateUI(counter: Int){
+        if (counter > 0){
+            binding.text.text = counter.toString()
+        }else{
+            binding.button.text = getString(R.string.start_service)
+            binding.text.text = ""
+            isCounterRunning = false
+        }
+    }
+
+    private fun sendMsgToService(messageId: Int){
+        val msg = Message.obtain(null, messageId, 0, 0)
+        msg.replyTo = myMessenger
+        try {
+            myService?.send(msg)
+        }catch (e: Exception){
+            Log.e(TAG, e.toString())
+        }
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/example/androidmaster2/MainService.kt
+++ app/src/main/java/com/example/androidmaster2/MainService.kt
@@ -1,22 +1,26 @@
 package com.example.androidmaster2
 
 import android.annotation.SuppressLint
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
+import android.content.ContentValues.TAG
 import android.content.Intent
-import android.os.Binder
 import android.os.Build
 import android.os.Handler
 import android.os.IBinder
 import android.os.Looper
+import android.os.Message
+import android.os.Messenger
+import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 @RequiresApi(Build.VERSION_CODES.O)
 class MainService : Service() {
 
-    private val binder = LocalBinder()
+    private lateinit var mMessenger: Messenger
     private val handler = Handler(Looper.getMainLooper())
     private val channelId = "CHANNEL_ID"
     private val notificationId = 1
@@ -28,15 +32,34 @@ class MainService : Service() {
     var isRunning = false
     private var counter = 21
 
-    inner class LocalBinder : Binder() {
-        fun getService(): MainService = this@MainService
+    @SuppressLint("HandlerLeak")
+    inner class IncomingHandler(): Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            var clientMessenger = msg.replyTo
+            var replyArg = 0
+
+            when(msg.what){
+                START_TIMER -> replyArg = startCountdown()
+                RESTART_TIMER -> replyArg = restartCountdown()
+                GET_CURRENT -> replyArg = getCurrentProgress()
+                IS_RUNNING -> replyArg = if (isRunning) 1 else 0
+                else -> super.handleMessage(msg)
+            }
+
+            val msg: Message = Message.obtain(null, msg.what, replyArg, 0)
+            try {
+                clientMessenger?.send(msg)
+            }catch (e: Exception){
+                Log.e(TAG, e.toString())
+            }
+        }
     }
 
     override fun onCreate() {
         super.onCreate()
 
         val channel = NotificationChannel(
-            channelId, "Foreground Service Channel",
+            channelId, "Foreground Messenger Service Channel(FMSC)",
             NotificationManager.IMPORTANCE_DEFAULT
         )
         notificationManager = getSystemService(NotificationManager::class.java)
@@ -44,10 +67,12 @@ class MainService : Service() {
 
         notificationIntent = Intent(this, MainActivity::class.java)
         pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE)
+
+        mMessenger = Messenger(IncomingHandler())
     }
 
     override fun onBind(intent: Intent?): IBinder? {
-        return binder
+        return mMessenger.binder
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -91,24 +116,26 @@ class MainService : Service() {
 
     @SuppressLint("ForegroundServiceType")
     private fun startForegroundService() {
-        val notification = NotificationCompat.Builder(this, channelId)
-            .setContentTitle("Service Notification")
-            .setContentText("Time: $counter")
-            .setSmallIcon(R.mipmap.ic_launcher)
-            .setContentIntent(pendingIntent)
-            .build()
-
-        startForeground(notificationId, notification)
+        startForeground(notificationId, getNotification())
     }
 
     private fun updateForegroundServiceCounter(counter: Int) {
-        val notification = NotificationCompat.Builder(this, channelId)
-            .setContentTitle("Service Notification")
-            .setContentText("Time: $counter")
+        notificationManager.notify(notificationId, getNotification())
+    }
+
+    private fun getNotification(): Notification{
+        return NotificationCompat.Builder(this, channelId)
+            .setContentTitle("Title")
+            .setContentText("Remaining time: $counter")
             .setSmallIcon(R.mipmap.ic_launcher)
             .setContentIntent(pendingIntent)
             .build()
+    }
 
-        notificationManager.notify(notificationId, notification)
+    companion object{
+        const val START_TIMER = 1
+        const val RESTART_TIMER = 2
+        const val GET_CURRENT = 3
+        const val IS_RUNNING = 4
     }
 }
